518d71dd7aa46ce5ba1904f55f2c6c4da826feed
Terence Lee <hone02@gmail.com>
2019-08-14 17:59:34 -0500

    add some output
    Co-authored-by: Terence Lee <hone02@gmail.com>
    Co-authored-by: Travis Longoria <longoria.public@gmail.com>

--- src/build.rs
+++ src/build.rs
@@ -24,28 +24,35 @@ struct Cli {
 pub fn main() -> Result<(), Error> {
     let args = Cli::from_args();
 
+    println!("Constructing Build::new");
     let build = Build::new(args.layers, args.platform, args.plan)?;
 
+    println!("Creating Middleware Layer");
     let mut middleware_layer = build.layers.add("middleware")?;
+    println!("Setting launch = true");
     middleware_layer.config(|c| {
         c.launch = true;
     })?;
 
+    println!("Writing middleware/index.js");
     let index_js = include_str!("../middleware/index.js");
     fs::write(middleware_layer.layer_path().join("index.js"), index_js)?;
 
+    println!("Writing middleware/package.json");
     let package_json = include_str!("../middleware/package.json");
     fs::write(
         middleware_layer.layer_path().join("package.json"),
         package_json,
     )?;
 
+    println!("Setting ENV VAR");
     middleware_layer
         .envs
         .launch
         .append_path
         .set_var("MIDDLEWARE_FUNCTION_URI", middleware_layer.layer_path());
 
+    println!("Running npm install");
     let mut command = Command::new("npm")
         .args(&["install", "--production"])
         .current_dir(middleware_layer.layer_path())
